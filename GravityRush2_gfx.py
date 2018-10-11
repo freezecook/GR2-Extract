b7755331e6ba3a0aa7a85ccb7dd3a8931d56646c
freezecook <freezecook@yahoo.com>
2018-10-11 00:49:29 -0400

    Materials- Pre Alpha
    Though commented out, this version is equipped to handle material data of kit01_base.gfx. Other files are untested.

--- GravityRush2_gfx.py
+++ GravityRush2_gfx.py
@@ -44,6 +44,193 @@ def noepyLoadModel(data, mdlList):
 	file.seek(0x14, NOESEEK_ABS)
 	file.seek(file.readUInt(), NOESEEK_ABS)
 	
+	'''
+	#material code starts here
+	print("Scanning for material info from " + hex(file.tell()))
+	dataFinder = file.readUInt()
+	while dataFinder != 17235971: #0x03000701
+		dataFinder = file.readUInt()
+		
+	#material data found; let's read our first texture name.
+	#file.seek(4,1) #this goes straight to the texture
+	
+	#navigate to textureID
+	file.seek(-28,1)
+	print("First Texture found at " + hex(file.tell()))
+	textureID = file.readUInt()
+	#check for magic numbers 27 lines
+	#(432 bytes + the 4 we just read) above.
+	file.seek(-436,1)
+	materialStart = file.readUInt()
+	matID = file.readUInt()
+	#backtrack
+	file.seek(428,1)
+	#navigate to texture name
+	file.seek(28,1)
+
+	complexMaterials = False
+	if materialStart == 3 or materialStart == 7:
+		complexMaterials = True
+	else:
+		matID = 0
+
+	finishedMaterials = False
+	materialComplete = False
+	textureIsNamed = False
+	textureJump = 432
+	texName = {}
+	textures = {}
+	materials = {}
+	#I could use a dictionary, but it's easier
+	#if I just use two arrays for this.
+	materialID = {}
+	materialAltID = {}
+
+	#set first materialID
+	materialID[0] = matID
+	materialAltID[0] = matID
+
+	matCount = 0
+	
+	#building materials
+	while finishedMaterials == False:
+		textures = {}
+		texCount = 0
+		#is the current material an alternate?
+		if matCount > 0:
+			if materialID[matCount -1] == materialID[matCount]:
+				#if so, we be careful about reading textures.
+				print("this material is an alternate")
+				if file.readUInt() == 0:
+					materialComplete = True
+				file.seek(-4,1)
+		#done checking for alts. now we'll build the material.
+		while materialComplete == False:   
+			i = 0
+			texName = {}
+			while textureIsNamed == False:
+				#checking if we have an extended name
+				if file.tell() % 16 == 0:
+					nextID = file.readUInt()
+					file.seek(-4,1)
+					if textureID == nextID - 2: #if we do, the string is complete.
+						textureIsNamed = True
+						continue
+				texChar = file.readByte()
+				if texChar != 0:
+					texName[i] = texChar
+					#print(texName[i])
+				else:
+					textureIsNamed = True
+				i = i + 1
+			#now that we have every character,
+			#we can construct the texture name.
+			textureIsNamed = False
+			textures[texCount] = ""
+			for char in texName:
+				textures[texCount] = textures[texCount] + chr(texName[char])
+			#push forward to next textureID
+			positionTest = file.tell() % 16
+			if positionTest != 0:
+				file.seek(16 - positionTest, 1)
+
+			texCount = texCount + 1
+			#repeat or end if there is no textureID.
+			nextID = file.readUInt()
+			file.seek(12,1)
+			altNextID = file.readUInt()
+			file.seek(-16,1)
+			print(hex(file.tell()) + ": texID = " + str(nextID) + " - " + str(textureID))
+			print(str(texCount))
+			if materialStart == 7 and texCount > 0 and textureID == nextID - 2 and materialAltID[matCount-1] == 399:
+				#I'm using a bad condition for this, but this entire loop
+				#needs a rewrite anyway.
+				textureID = nextID
+				file.seek(28,1)
+				print("heavy at " + hex(file.tell()))
+			elif materialStart == 7 and texCount > 0 and textureID == nextID - 2:
+				textureID = nextID
+				file.seek(60,1)
+				print("light at " + hex(file.tell()) )
+			elif textureID == nextID - 2:
+				textureID = nextID
+				file.seek(28,1)
+			elif textureID == altNextID - 3:
+				textureID = altNextID
+				file.seek(44,1)
+			elif complexMaterials == False:
+				materialComplete = True
+				finishedMaterials = True
+			else:
+				materialComplete = True
+			
+		#end of Material/texture loop
+		materialComplete = False
+		textureJump = 432
+		#print(texName)
+		print(textures)
+		materials[matCount] = textures
+		
+		#check for the next magic number.
+		positionTest = file.tell() % 16
+		if positionTest != 0:
+			file.seek(16 - positionTest, 1)
+
+		for x in range(7):#while magic number isn't found
+			checkedInt = file.readUInt()
+			if checkedInt == 3 or checkedInt == 7:
+				#set materialID and materialAltID
+				materialStart = checkedInt
+				materialID[matCount + 1] = file.readUInt()
+				if materialID[matCount] == materialID[matCount + 1]:
+					file.seek(20,1)
+					materialAltID[matCount + 1] = file.readUInt()
+					file.seek(-24,1)
+				else:
+					materialAltID[matCount + 1] = materialID[matCount + 1]
+				file.seek(-4,1)
+				#we found another material. let's
+				#do some setup and go back to the main loop.
+				if materialAltID[matCount + 1] == 1073741824:
+					#this is for the "inner" material.
+					#It defaults to 432 every iteration. 
+					textureJump = 208
+					print("altering jump distance")
+					#BUT WAIT! There's more! 
+					#Somehow, I'll have to predict the actual ID for this value.
+					materialID[matCount + 1] = materialID[matCount] + 10
+					materialAltID[matCount + 1] = materialID[matCount + 1]
+				print("found new material! " + hex(file.tell()))
+				file.seek(textureJump - 4,1)
+				textureID = file.readUInt()
+				file.seek(28,1)
+				print("first texture at " + hex(file.tell()))
+				break
+			elif x == 6:
+				#no more materials; mission complete!
+				print("all materials COMPLETE!")
+				finishedMaterials = True
+			else:
+				#nothing definitive.
+				#let's jump ahead to read the next Int.
+				file.seek(12,1)
+		matCount = matCount + 1
+	#end of All Materials loop
+	print("PRINTING MATERIALS")
+	trueMaterials = {}
+	for mat in materials:
+		#finally creating materials that Noesis can understand!
+		#if 0 in materials[mat]:
+			#trueMaterials[mat] = NoeMaterial("Mat"+ str(mat), materials[mat][0])
+			
+			#if 1 in materials[mat]:
+				#trueMaterials[mat].setNormalTexture(materials[mat][1])
+		#else:
+			#trueMaterials[mat] = NoeMaterial("Mat"+ str(mat), "")
+		print(materials[mat])
+		file.seek(-4, NOESEEK_REL)
+	'''
+	#beginning of mesh code
 	print("Scanning for mesh metadata from " + hex(file.tell()))
 	dataFinder = file.readUInt()
 	while dataFinder != 2661065091 and dataFinder != 2292097411 and dataFinder != 2661196163 and dataFinder != 10396035:
@@ -60,6 +247,7 @@ def noepyLoadModel(data, mdlList):
 	modelDictionary = {}
 	modelVertices = {}
 	faceBehaviors = {}
+	meshMaterials  = {}
 	dictionaryComplete = False
 
 	while dictionaryComplete == False:
@@ -67,6 +255,7 @@ def noepyLoadModel(data, mdlList):
 		vertexCount = file.readUInt()
 		checkedInt = file.readUInt()
 		if checkedInt >= 3 and checkedInt <= 6:
+			#starting a new mesh.
 			faceIndex = 1
 			modelDictionary["Vertex" + str(index)] = vertexCount
 			file.seek(8, NOESEEK_REL) #using vertex type identifier
@@ -89,25 +278,72 @@ def noepyLoadModel(data, mdlList):
 				modelVertices["Mesh" + str(index)] = 20
 			else:
 				modelVertices["Mesh" + str(index)] = 32
-			file.seek(16, NOESEEK_REL)
-		else:
+			file.seek(16, NOESEEK_REL) #set to 8 if reading materials
+		else: 
+			#this is a submesh.
 			index = index - 1
 			submesh = True
+			#we're gonna step back a bit for the material assignment.
+			#file.seek(-8, NOESEEK_REL) #remove if not processing materials
 		
+		'''
+		#get materialID from mesh metadata. This is either 1 more or 1 less 
+		#than the materialAltID's we collected earlier.
+		thisMaterial = file.readUInt()
+			
+		#remember, there are no shortcuts for submeshes!
+		#also, some meshes use an alternate material as the base.
+		print("Mesh" + str(index))
+		print("at " + hex(file.tell()))
+		print("material ID: " + hex(thisMaterial))
+		for mat in materialAltID:
+			print(hex(materialAltID[mat]))
+			if thisMaterial + 1 == materialAltID[mat]:
+				meshMaterials["Mesh" + str(index) + "_" + str(faceIndex)] = mat
+				break
+			elif (thisMaterial - 1 == materialAltID[mat]) or (thisMaterial >= materialID[mat] and thisMaterial <= materialAltID[mat]):
+				count = mat
+				while count >= 0:
+					if materialID[count] == materialAltID[count]:
+						break
+					count = count - 1
+				meshMaterials["Mesh" + str(index) + "_" + str(faceIndex)] = count
+				break
+			elif mat + 1 < len(materialAltID):
+				if thisMaterial >= materialID[mat] and thisMaterial <= materialAltID[mat + 1]:
+					#this accounts for the oddball material used for Kat's grav wisps
+					print("Using oddball mat")
+					meshMaterials["Mesh" + str(index) + "_" + str(faceIndex)] = mat	
+			else:
+				if ("Mesh" + str(index) + "_" + str(faceIndex) in meshMaterials) == False: 
+					print("WARNING: no suitable material found. Setting to 0")
+					meshMaterials["Mesh" + str(index) + "_" + str(faceIndex)] = 0
+		print("Determined material: " + str(meshMaterials["Mesh" + str(index) + "_" + str(faceIndex)]))
+		file.seek(4, NOESEEK_REL)
+		'''
+		
+		#Getting face data
 		modelDictionary["Face" + str(index) + "_" + str(faceIndex)] = file.readUShort()
 		if modelDictionary["Face" + str(index) + "_" + str(faceIndex)] == 0:
+			#either there's no further face data, or this was an error check.
+			#It may not be needed anymore.
 			del(modelDictionary["Face" + str(index) + "_" + str(faceIndex)])
 			break
 		file.seek(14, NOESEEK_REL)
-		behaviorIndices = file.readUShort()
+		#this tells me how much face data is repeated. I can use it to 
+		#guess where the next set of useful data begins.
+		behaviorIndices = file.readUShort() 
 		if behaviorIndices != 3 * modelDictionary["Face" + str(index) + "_" + str(faceIndex)]:
 			faceBehaviors["Mesh" + str(index) + "_" + str(faceIndex)] = 6
 		else:
 			faceBehaviors["Mesh" + str(index) + "_" + str(faceIndex)] = 18
 		file.seek(6, NOESEEK_REL)
-		print(hex(file.tell()))
+		#print(hex(file.tell()))
 		faceIndices = file.readUInt()
 		if modelDictionary["Face" + str(index) + "_" + str(faceIndex)] * 3 != faceIndices:
+			#there is no further face data. I can stop looking for new meshes.
+			#No need to worry about verts. It involves some logic sports here, 
+			#but verts already won't be set if there's no face data.
 			dictionaryComplete = True
 			del(modelDictionary["Face" + str(index) + "_" + str(faceIndex)])
 		file.seek(12, NOESEEK_REL)
@@ -115,16 +351,21 @@ def noepyLoadModel(data, mdlList):
 			file.seek(12, NOESEEK_REL)
 		else:
 			file.seek(-4, NOESEEK_REL)
-		print(hex(file.tell()))
+		#print(hex(file.tell()))
 		faceIndex = faceIndex + 1
 		index = index + 1
 
+	print("MODEL DICTIONARY")
 	print(modelDictionary)
+	print("VERTEX TYPES")
 	print(modelVertices)
+	print("FACE BEHAVIORS")
 	print(faceBehaviors)
+	#print("MATERIALS")
+	#print(meshMaterials)
 
 	file.seek(vertStart, NOESEEK_ABS)
-
+	
 	meshes = []
 	
 	while fileComplete == False:
@@ -296,4 +537,9 @@ def noepyLoadModel(data, mdlList):
 	mdl = NoeModel(meshes)
 	mdlList.append(mdl)
 	
+	'''
+	#printing materials for future reference.
+	for mat in materials:
+		print(materials[mat])
+	'''
 	return 1
